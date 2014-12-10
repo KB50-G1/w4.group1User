fe382c4dbf443e2051b757fbc9aab7fd3e0c7845
anglean2002 <anglean2002@hotmail.com>
2014-12-10 15:18:18 +0100

    gallery work!

--- app/src/main/java/pidal/alfonso/w4group1user/InfoFragment.java
+++ app/src/main/java/pidal/alfonso/w4group1user/InfoFragment.java
@@ -1,11 +1,19 @@
 package pidal.alfonso.w4group1user;
 
 import android.app.Fragment;
+import android.content.Context;
+import android.content.res.TypedArray;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.BaseAdapter;
+import android.widget.Gallery;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 
 /**
@@ -17,6 +25,16 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class InfoFragment extends Fragment {
+    //---the images to display---
+    Integer[] imageIDs = {
+            R.drawable.pic1,
+            R.drawable.pic2,
+            R.drawable.pic3,
+            R.drawable.pic4
+    };
+
+    private Gallery gallery;
+
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -53,17 +71,89 @@ public class InfoFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
 
+    public class ImageAdapter extends BaseAdapter
+    {
+        Context context;
+        int itemBackground;
+
+        public ImageAdapter(Context c)
+        {
+            context = c;
+            //---setting the style---
+            /*
+            TypedArray a = getActivity().obtainStyledAttributes(R.styleable.Gallery1);
+            itemBackground = a.getResourceId(R.styleable.Gallery1_android_galleryItemBackground,
+                    0);
+            a.recycle();*/
+        }
+
+        //---returzns the number o images---
+        public int getCount() {
+            return imageIDs.length;
+        }
+
+        //---returns the item---
+        public Object getItem(int position) {
+            return position;
+        }
+
+        //---returns the ID of an item---
+        public long getItemId(int position) {
+            return position;
+        }
+
+        //---returns the ImageView view---
+        public View getView(int position, View convertView,
+                            ViewGroup parent) {
+            ImageView imageView;
+            if (convertView == null) {
+                imageView = new ImageView(context);
+                imageView.setImageResource(imageIDs[position]);
+                imageView.setScaleType(
+                        ImageView.ScaleType.FIT_XY);
+                imageView.setLayoutParams(
+                        new Gallery.LayoutParams(150, 120));
+            } else {
+                imageView = (ImageView) convertView;
+            }
+            imageView.setBackgroundResource(itemBackground);
+            return imageView;
+        }
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_info, container, false);
+        gallery = (Gallery) rootView.findViewById(R.id.gallery1);
+
+        gallery.setAdapter(new ImageAdapter(getActivity()));
+        gallery.setOnItemClickListener(new OnItemClickListener()
+        {
+            public void onItemClick(AdapterView parent, View v,
+                                    int position, long id)
+            {
+                Toast.makeText(getActivity().getBaseContext(),
+                        "pic" + (position + 1) + " selected",
+                        Toast.LENGTH_SHORT).show();
+
+                //---display the images selected---
+                ImageView imageView =
+                        (ImageView) getActivity().findViewById(R.id.image1);
+                imageView.setImageResource(imageIDs[position]);
+            }
+        });
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_info, container, false);
+        return rootView;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
